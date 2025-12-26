5f2f60a1c28d6c2bf7cee6f71aa58a396e855aff
chaitanyakelkar27 <2023.chaitanya.kelkar@ves.ac.in>
2025-12-27 00:08:17 +0530

    made changes

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { stepsData } from '../assets/assets'
-import motion from "motion/react"
+import { motion } from "motion/react"
 
 const Steps = () => {
     return (
