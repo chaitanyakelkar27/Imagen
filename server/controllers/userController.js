cb8323a2e511bc534ae5ca7e9e38e5afb1f154dd
chaitanyakelkar27 <2023.chaitanya.kelkar@ves.ac.in>
2025-12-27 21:21:37 +0530

    Created UserModel and RegisterUser function

---
+++ server/controllers/userController.js
@@ -0,0 +1,29 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.json({ success: false, message: 'Please provide all required fields' });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const userData = {
+            name,
+            email,
+            password: hashedPassword
+        }
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.json({ success: true, token, user: { name: user.name } });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Error in registering user',
+            error
+        });
+    }
+};
