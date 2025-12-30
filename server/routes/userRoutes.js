3fd8e30a7710468efaa3371db9d3e38320a30910
chaitanyakelkar27 <2023.chaitanya.kelkar@ves.ac.in>
2025-12-30 17:14:30 +0530

    feat: add Google authentication callback to user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, userCredits } from '../controllers/userController.js';
+import { registerUser, loginUser, userCredits, googleCallback } from '../controllers/userController.js';
 import userAuth from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -7,5 +7,13 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/credits', userAuth, userCredits);
+
+router.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: process.env.FRONTEND_URL,
+        session: false
+    }),
+    googleCallback
+);
 
 export default router;
