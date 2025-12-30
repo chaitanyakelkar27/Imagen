72c64763accad193c116407fd4ab7cd3495090d4
chaitanyakelkar27 <2023.chaitanya.kelkar@ves.ac.in>
2025-12-30 16:10:26 +0530

    Added dependencies and created passport.js file

---
+++ server/config/passport.js
@@ -0,0 +1,57 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import userModel from "../models/userModel.js";
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+            scope: ["profile", "email"]
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let existingUser = await userModel.findOne({ googleId: profile.id });
+                if (existingUser) {
+                    return done(null, existingUser);
+                }
+                existingUser = await userModel.findOne({ email: profile.emails[0].value });
+                if (existingUser) {
+                    existingUser.googleId = profile.id;
+                    existingUser.authProvider = 'google';
+                    existingUser.profilePicture = profile.photos[0]?.value;
+                    await existingUser.save();
+                    return done(null, existingUser);
+                }
+
+                const newUser = await userModel.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id,
+                    authProvider: 'google',
+                    profilePicture: profile.photos[0]?.value
+                });
+                return done(null, newUser);
+            } catch (error) {
+                return done(error, null);
+            }
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user_id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await userModel.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error, null);
+    }
+});
+
+export default passport;
+
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    googleId: { type: String, unique: true, sparse: true },
+    profilePic: { type: String },
+    authProvider: { type: String, enum: ['local', 'google'], default: 'local' },
     creditBalance: { type: Number, default: 5 }
 });
 
