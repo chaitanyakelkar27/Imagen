d1c213f549e94535e8ee68166b30e674d0eb526a
chaitanyakelkar27 <2023.chaitanya.kelkar@ves.ac.in>
2025-12-31 20:05:04 +0530

    feat: add user image retrieval and management routes to imageRouter

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { generateImage } from '../controllers/imageController.js';
+import { generateImage, getUserImages, deleteImage, deleteMultipleImages, toggleFavorite, getUserStats } from '../controllers/imageController.js';
 import userAuth from '../middlewares/auth.js';
 
 const imageRouter = express.Router();
 
 imageRouter.post('/generate-image', userAuth, generateImage);
+imageRouter.get('/user-images', userAuth, getUserImages);
+imageRouter.delete('/delete/:imageId', userAuth, deleteImage);
+imageRouter.post('/delete-multiple', userAuth, deleteMultipleImages);
+imageRouter.patch('/favorite/:imageId', userAuth, toggleFavorite);
+imageRouter.get('/stats', userAuth, getUserStats);
 
 export default imageRouter;
